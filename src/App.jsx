02b42035fd5480009e907855b105700c64da54c7
Varutcha Chanchampa <zonasysasia@gmail.com>
2022-06-08 09:04:18 +0700

    update link lms old and new

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,74 @@
-import { useState } from 'react'
-import logo from './images/logo.png'
-import lmsLogo from './images/lmsLogo.png'
-import './App.css'
-import Footer from './Footer'
+import { useState } from "react";
+import logo from "./images/logo.png";
+import lmsLogo from "./images/lmsLogo.png";
+import "./App.css";
+import Footer from "./Footer";
+import lmsOld from "./images/lmsOld.png";
+import lmsNew from "./images/lmsNew.png";
+import Head1 from "./images/head1.png";
+import BannerLMS from "./images/BannerLMS.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App-header">
-      {/* <img src="https://lms.rmutsv.ac.th/img/banner.JPG" alt="" className='img-fluid mb-3 rounded-2 shadow-lg' /> */}
-      <img src={lmsLogo} alt="" className='img-fluid mb-3' />
-      <hr />
-      {/* <img src={logo} width="200" alt="" /> */}
-      {/* <h1>มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย</h1> */}
-      <h5 className='text-dark rounded bg-warning p-3 shadow mt-2 mb-2'><i className="fa-solid fa-bullhorn text-light"></i> {' '}ข่าวประชาสัมพันธ์</h5>
-      <div className="row">
-        <div className="col-lg-3 col-sm">
-        </div>
-        <div className="col-lg-3 col-sm">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title text-light text-center bg-primary rounded-pill p-3">เข้าสู่ระบบ LMS ใหม่</h5>
-              <p className="text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-sm">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title text-light text-center bg-primary rounded-pill p-3">เข้าสู่ระบบ LMS เก่า</h5>
-              <p className="text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-sm"></div>
+    <>
+      <div
+        style={{ backgroundColor: "#021B54" }}
+        className="container-fluid shadow d-flex text-warning text-center justify-content-center align-items-center"
+        id="header"
+      >
+        {/* <img src={BannerLMS} className="img-fluid" alt="" /> */}
       </div>
-      <Footer />
-    </div>
-  )
+
+      <div className="App-header">
+        {/* <img src="https://lms.rmutsv.ac.th/img/banner.JPG" alt="" className='img-fluid mb-3 rounded-2 shadow-lg' /> */}
+
+        <hr />
+        {/* <img src={logo} width="200" alt="" /> */}
+        {/* <h1>มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย</h1> */}
+        <h5 className="text-light rounded bg-secondary p-3 shadow mt-2 mb-2">
+          <i className="fa-solid fa-bullhorn text-light"></i> ข่าวประชาสัมพันธ์
+        </h5>
+        <div className="row">
+          <div className="col-lg-3 col-md"></div>
+          <div className="col-lg-3 col-sm mt-3 mb-3 p-3">
+            <div className="card">
+              <a href="https://lms65.rmutsv.ac.th">
+                <img src={lmsNew} alt="" className="img-fluid card-img-top" />
+              </a>
+              <div className="card-body text-center">
+                <a
+                  href="https://lms65.rmutsv.ac.th"
+                  className="btn btn-warning btn-xs btn-block"
+                >
+                  เข้าสู่ระบบ
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-sm mb-3 mt-3 p-3">
+            <div className="card">
+              <a href="https://lms59.rmutsv.ac.th">
+                <img src={lmsOld} alt="" className="img-fluid card-img-top" />
+              </a>
+              <div className="card-body text-center">
+                <a
+                  href="https://lms59.rmutsv.ac.th"
+                  className="btn btn-xs btn-block"
+                  style={{ backgroundColor: "#68A4C4" }}
+                >
+                  เข้าสู่ระบบ
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md"></div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Contents.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Contents() {
+  return (
+    <div className="row">
+      <div className="col-sm">
+        <strong>Contact us</strong>
+        <ul className="mt-2">
+          <li className="list-unstyled">
+            <p>
+              <img src={AritLogo} width="50" alt="" />
+              สำนักวิทยบริการและเทคโนโลยีสารสนเทศ
+              <br />
+            </p>
+            <p>
+              <img
+                src="https://www.rmutsv.ac.th/sites/default/files/image/Logo-RUTS.png"
+                className="rounded shadow-lg"
+                width={150}
+                alt=""
+              />
+              {/* มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย<br /> */}
+              <br />
+              เลขที่ 1 ถนนราชดำเนินนอก ตำบลบ่อยาง อำเภอเมือง
+              <br />
+              จังหวัดสงขลา 90000
+              <br />
+              โทรศัพท์ 0-7431-7146, 0-7431-7100 ต่อ 1160 - 1165 โทรสาร
+              0-743-17147
+              <br />
+              <i
+                className="fa-brands fa-line text-success"
+                style={{ fontSize: "24px" }}
+              ></i>{" "}
+              ช่องทาง Line @lhp1366x
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="col-sm">
+        <img src={MoodleLogo} width={150} alt="" />
+        <p>
+          Pages <br />
+          RMUTSV เว็บไซต์มหาวิทยาลัย RMUTSV e-Passport e-Service
+          <br />
+          RMUTSV mail สำหรับบุคลากร/อาจาย์ <br />
+          RMUTSV mail สำหรับนักศึกษา
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Contents
